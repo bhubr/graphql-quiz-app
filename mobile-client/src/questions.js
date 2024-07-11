5fbb52c9f704b37bdd6bb9f5c8d066c544374163
Benoît Hubert <benoithubert@gmail.com>
2024-07-11 11:31:12 +0200

    move mobile app code

---
+++ mobile-client/src/questions.js
@@ -0,0 +1,36 @@
+export const questions = [
+  {
+    title: "How to create a new branch with Git?",
+    answers: [
+      {
+        text: "git checkout -b <branch>",
+        correct: true,
+      },
+      {
+        text: "git create <branch>",
+        correct: false,
+      },
+      {
+        text: "git branch <branch>",
+        correct: true,
+      },
+      {
+        text: "git switch -c <branch>",
+        correct: true,
+      },
+    ],
+  },
+  {
+    title: "How to rename a branch?",
+    answers: [
+      {
+        text: "git rename <oldbranch> <newbranch>",
+        correct: false,
+      },
+      {
+        text: "git branch -m <newbranch>",
+        correct: true,
+      },
+    ],
+  },
+];
